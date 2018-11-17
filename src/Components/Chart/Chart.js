c203e02a6b3091732cee7bf708cef609eb68b7e6
Song <yousonghou@yahoo.com>
2018-11-17 17:14:20 +0000

    add label and resize the pie graph

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -8,11 +8,14 @@ class Chart extends Component {
     return (
       <div>
         <PieChart
-            data={[
-              { key: 'A', value: 100 },
-              { key: 'B', value: 200 },
-              { key: 'C', value: 50 }
-            ]}
+          labels
+          size={400}
+          innerHoleSize={250}
+          data={[
+            { key: '20%', value: 100, color: '#3cb371' },
+            { key: '55%', value: 200, color: '#ffa500' },
+            { key: '10%', value: 50, color: '#ff0000' }
+          ]}
         />
       </div>
     );

23c447ffad82afabe656d44611254d6cfccb70ee
Song <yousonghou@yahoo.com>
2018-11-17 15:48:43 +0000

    add pie graph

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+import {PieChart} from 'react-easy-chart';
+
 class Chart extends Component {
   render() {
+      
     return (
-      <div className="App">
-        
+      <div>
+        <PieChart
+            data={[
+              { key: 'A', value: 100 },
+              { key: 'B', value: 200 },
+              { key: 'C', value: 50 }
+            ]}
+        />
       </div>
     );
   }

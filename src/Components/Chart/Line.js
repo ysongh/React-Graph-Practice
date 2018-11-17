9266c0343b2113fa7a1350a1656d046348c37462
Song <yousonghou@yahoo.com>
2018-11-17 16:28:28 +0000

    change data on line graph

--- src/Components/Chart/Line.js
+++ src/Components/Chart/Line.js
@@ -3,32 +3,33 @@ import React, { Component } from 'react';
 import {LineChart} from 'react-easy-chart';
 
 class Line extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        data: [
+          [
+            { x: 1, y: 20 },
+            { x: 2, y: 10 },
+            { x: 3, y: 25 }
+          ], [
+            { x: 1, y: 10 },
+            { x: 2, y: 12 },
+            { x: 3, y: 4 }
+          ]
+        ]
+    };
+  }
   render() {
       
     return (
       <div>
         <LineChart
-            axisLabels={{x: 'Total Revenue', y: 'January'}}
-            margin={{top: 10, right: 30, bottom: 50, left: 70}}
-            yType={'time'}
             axes
-            width={500}
-            height={500}
-            data={[
-              [
-                { x: 10, y: '1-Jan-15' },
-                { x: 20, y: '10-Jan-15' },
-                { x: 40, y: '21-Jan-15' },
-                { x: 80, y: '31-Jan-15' }
-              ], [
-                { x: 0, y: '1-Jan-15' },
-                { x: 15, y: '10-Jan-15' },
-                { x: 20, y: '21-Jan-15' },
-                { x: 25, y: '31-Jan-15' }
-              ]
-            ]}
+            width={350}
+            height={350}
+            data={this.state.data}
         />
-      </div>
+    </div>
     );
   }
 }

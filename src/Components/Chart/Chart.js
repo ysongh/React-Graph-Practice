ed2890317e6d240fee89d677c70c85f6cfd2e9c2
Song <yousonghou@yahoo.com>
2018-11-17 15:16:47 +0000

    setup chart

---
+++ src/Components/Chart/Chart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Chart extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default Chart;

4740d2238939318af17f10afd9dac693c9898e1e
Song <yousonghou@yahoo.com>
2018-11-17 17:55:15 +0000

    add legend to pie graph

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 import {PieChart} from 'react-easy-chart';
+import {Legend} from 'react-easy-chart';
 
 class Chart extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        data: [
+            { key: '20%', value: 100, color: '#3cb371', name: 'Doing Good' },
+            { key: '55%', value: 200, color: '#ffa500' , name: 'Need Improvement' },
+            { key: '10%', value: 50, color: '#ff0000' , name: 'Not So Good' }
+          ]
+    };
+  }
+  
   render() {
+    
+    const config = [
+      {color: '#3cb371'},
+      {color: '#ffa500'},
+      {color: '#ff0000'}
+    ];
       
     return (
       <div>
@@ -11,12 +29,9 @@ class Chart extends Component {
           labels
           size={400}
           innerHoleSize={250}
-          data={[
-            { key: '20%', value: 100, color: '#3cb371' },
-            { key: '55%', value: 200, color: '#ffa500' },
-            { key: '10%', value: 50, color: '#ff0000' }
-          ]}
+          data={this.state.data}
         />
+        <Legend data={this.state.data} dataId={'name'} config={config} horizontal />
       </div>
     );
   }
